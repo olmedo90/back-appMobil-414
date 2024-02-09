b8f37f6e87d2129476fc0c3d0eeec616c9813221
olmedo90 <olmedito.90@gmail.com>
2024-02-08 21:08:22 -0400

    config mongo atlas

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const PORT = process.env.PORT || 5000;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/app-mobil";
+  process.env.MONGODB_URI || "mongodb+srv://olmedo:olmedo@cluster0.js6hz4q.mongodb.net/?retryWrites=true&w=majority";
+  // process.env.MONGODB_URI || "mongodb://localhost/app-mobil";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
